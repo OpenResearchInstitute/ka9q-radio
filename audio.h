83bd0d856668c1ac2cf6abf5dcf1c75892e55563
karn <karn>
2016-10-13 23:07:15 +0000

    definitions for audio.c

---
+++ audio.h
@@ -0,0 +1,24 @@
+#ifndef _AUDIO_H
+#define _AUDIO_H 1
+
+#include <alsa/asoundlib.h>
+
+struct audio {
+  char *name;
+  snd_pcm_t *handle;
+  int samprate;
+  int channels; // Number of channels in current buffer
+  int underrun;
+  int echo;     // If set, echo to standard output
+};
+
+extern struct audio Audio;
+
+int audio_change_parms(unsigned samplerate,int channels,int L);
+int audio_out_done();
+int put_stereo_audio(complex float *,int,float);
+int put_mono_audio(float *,int,float);
+
+#endif
+
+
