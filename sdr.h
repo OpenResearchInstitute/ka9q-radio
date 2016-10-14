bb43c29276e6c9770d28ae12766a58d40aa4a584
karn <karn>
2016-10-14 04:35:50 +0000

    add declaration for mirics_gain()

--- sdr.h
+++ sdr.h
@@ -1,10 +1,11 @@
-// $Id$
+// $Id: sdr.h,v 1.2 2016/10/13 23:59:21 karn Exp karn $
 #ifndef _SDR_H
 #define _SDR_H 1
 
 #include <complex.h>
 #undef I
 
+int mirics_gain(double f,int gr,int *bb, int *lna,int *mix);
 int front_end_init(int,int,int);
 float get_adc(complex float *,int);
 void closedown(int);
