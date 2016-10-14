1236ec3e796155a508ec315c1bec7fc45442bd9b
karn <karn>
2016-10-14 04:34:35 +0000

    simplify, remove unneeded includes

--- gr.c
+++ gr.c
@@ -1,18 +1,12 @@
-// $Id: gr.c,v 1.2 2016/10/13 23:34:07 karn Exp karn $
+// $Id: gr.c,v 1.3 2016/10/14 01:17:27 karn Exp karn $
 // Mirics default gain reduction tables
 // Mirics SDR API specification, section 5
-#include <assert.h>
-#include "dsp.h"
 int mirics_gain(double f,int gr,int *bb, int *lna,int *mix){
-  *bb = *lna = *mix = 0;
   if(gr < 0)
     gr = 0;
 
-  if(f < 60e6){
-    if(gr >= 35)
-      *lna = 24;
-  } else if(f < 120e6){
-    // Same as < 60 MHz, why is this separate?
+  *bb = *lna = *mix = 0;
+  if(f < 120e6){ // Also for < 60 MHz in original reference
     if(gr >= 35)
       *lna = 24;
   } else if(f < 250e6){
@@ -30,9 +24,10 @@ int mirics_gain(double f,int gr,int *bb, int *lna,int *mix){
   }
   // If the IF can't handle the rest, turn off the mixer too
   gr -= *lna;
-  if(gr > 59)
+  if(gr > 59){
     *mix = 19;
-  gr -= *mix;
-  *bb = min(gr,59);
-  return 0;
+    gr -= 19;
+  }
+  *bb = gr > 59 ? 59 : gr;
+  return *bb + *mix + *lna;
 }
