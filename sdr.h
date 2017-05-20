df8c64729a8cbd54537f84eb8c9ab9c786aa871e
karn <karn>
2017-05-20 01:18:04 +0000

    Change args to mirics_gain

--- sdr.h
+++ sdr.h
@@ -1,13 +1,13 @@
-// $Id: sdr.h,v 1.2 2016/10/13 23:59:21 karn Exp karn $
+// $Id: sdr.h,v 1.3 2016/10/14 04:35:50 karn Exp karn $
 #ifndef _SDR_H
 #define _SDR_H 1
 
 #include <complex.h>
 #undef I
 
-int mirics_gain(double f,int gr,int *bb, int *lna,int *mix);
+int mirics_gain(double f,int gr,char *bb, char *lna,char *mix);
 int front_end_init(int,int,int);
-float get_adc(complex float *,int);
+int get_adc(short *,int);
 void closedown(int);
 
 #endif
