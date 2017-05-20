5b398d86150347be2c03ffc3ac3e558e7710dbb2
karn <karn>
2017-05-20 01:14:08 +0000

    rename fm_squelch to fm_sr fix snr calculation

--- fm.c
+++ fm.c
@@ -1,4 +1,4 @@
-// $Id$
+// $Id: fm.c,v 1.2 2016/10/13 23:29:54 karn Exp karn $
 // FM demodulation and squelch
 #include <assert.h>
 #include <limits.h>
@@ -13,28 +13,27 @@
 #include "fm.h"
 #include "audio.h"
 
-// FM demodulator
-float squelch_fm(complex float *buffer,int L){
-  float fm_variance,fm_amplitude;
-  float magnitudes[L];
+// Estimate FM SNR
+float fm_snr(complex float *buffer,int L){
+  float fm_variance;
   int n;
   
-  // Find average amplitude
-  fm_amplitude = 0;
-  for(n=0;n<L;n++)
-    fm_amplitude += magnitudes[n] = cabsf(buffer[n]);
-  fm_amplitude /= L;
-  Demod.amplitude = fm_amplitude;
-  
-  // Find variance
-  fm_variance = 0;
+  // Find average magnitude and magnitude^2
+  // Approximate because magnitude has a chi-squared distribution with 2 degrees of freedom
+  float avg_squares = 0;
+  float avg = 0;
   for(n=0;n<L;n++){
-    float deviation = magnitudes[n] - fm_amplitude;
-    fm_variance += deviation * deviation;
+    float magsq = cnrmf(buffer[n]); // I^2 + Q^2
+    avg_squares += magsq;
+    avg += sqrtf(magsq);            // magnitude
   }
-  fm_variance /= (L-1);
-  // Find SNR, apply squelch
-  return fm_amplitude * fm_amplitude/(2*fm_variance);
+  avg_squares /= L; // Average square magnitude
+  avg /= L;         // Average magnitude
+  Demod.amplitude = avg;
+  fm_variance = avg_squares - avg*avg;
+  
+  // Find SNR
+  return avg*avg/(2*fm_variance) - 1;
 }
 
 
