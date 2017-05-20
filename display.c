5938464a66c3e42afafc2b126e8c3b7831b3a025
karn <karn>
2017-05-20 01:16:43 +0000

    multicast version, new handling of first_LO

--- display.c
+++ display.c
@@ -1,4 +1,4 @@
-// $Id: display.c,v 1.4 2016/10/14 07:37:13 karn Exp karn $
+// $Id: display.c,v 1.5 2017/05/11 10:32:06 karn Exp karn $
 // Thread to display internal state of 'radio' command on command line 
 #include <assert.h>
 #include <limits.h>
@@ -15,20 +15,22 @@
 #include "dsp.h"
 
 void *display(void *arg){
-  fprintf(stderr,"Mode        Frequency         First LO     First IF  Offset    IF1    IF2  AFgain  FM SNR  D/A Under\n");
+  fprintf(stderr,"Mode        Frequency         First LO     First IF  Offset    IF1    IF2  AFgain     SNR  D/A Under    DCI    DCQ\n");
   fprintf(stderr,"                   Hz               Hz           Hz      Hz     dB     dB      dB      dB\n");
   for(;;){
-    fprintf(stderr,"%-5s%'16.2f%'17.2f%'13.2f%'8.2f%7.1f%7.1f%8.1f%8.2f%11d\r",
+    fprintf(stderr,"%-5s%'16.2f%'17.2f%'13.2f%'8.2f%7.1f%7.1f%8.1f%8.2f%11d%7.1f%7.1f\r",
 	    Modes[Demod.mode].name,
-	    Demod.first_LO - Demod.second_LO + Modes[Demod.mode].dial,
-	    Demod.first_LO,
+	    get_first_LO() - get_second_LO(0) + Modes[Demod.mode].dial,
+	    get_first_LO(),
 	    -get_second_LO(0),
 	    Modes[Demod.mode].dial,
-	    power2dB(Demod.energy),
+	    power2dB(crealf(Demod.power) + cimagf(Demod.power)),
 	    voltage2dB(Demod.amplitude),
 	    voltage2dB(Demod.gain),
 	    power2dB(Demod.snr),
-	    Audio.underrun);
+	    Audio.underrun,
+	    crealf(Demod.DC_offset),
+	    cimagf(Demod.DC_offset));
     usleep(100000); // 10 Hz update
   }
   pthread_exit(0);
