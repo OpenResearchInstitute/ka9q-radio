9412ee0c9d2bd094fddac8e2df83b6529e90e5cf
karn <karn>
2017-05-20 01:15:07 +0000

    rename squelch_fm -> fm_snr

--- fm.h
+++ fm.h
@@ -1,9 +1,9 @@
-// $Id$
+// $Id: fm.h,v 1.2 2016/10/13 23:31:14 karn Exp karn $
 #ifndef _FM_H
 #define _FM_H 1
 
 #include "radio.h"
 
-float squelch_fm(complex float *,int);
+float fm_snr(complex float *,int);
 int do_fm(float *output,complex float *buffer,int L,complex float *state);
 #endif
