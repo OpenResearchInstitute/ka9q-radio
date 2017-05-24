534fd643eedfa0f22ae267aeeca15e818476fea1
karn <karn>
2017-05-24 09:01:07 +0000

    add ^L (refresh) and n (sample noise level) commands display some i/q imbalance stuff

--- display.c
+++ display.c
@@ -28,7 +28,7 @@ void *display(void *arg){
   noecho();
   fw = newwin(5,30,0,0);
   sig = newwin(6,20,6,0);
-  sdr = newwin(6,30,6,25);
+  sdr = newwin(7,30,6,25);
 
   for(;;){
     mvwprintw(fw,0,0,"Frequency   %'17.2f",get_first_LO() - get_second_LO(0) + Modes[Demod.mode].dial);
@@ -46,32 +46,43 @@ void *display(void *arg){
     
     mvwprintw(sdr,0,0,"I offset %7.1f",crealf(Demod.DC_offset));
     mvwprintw(sdr,1,0,"Q offset %7.1f",cimagf(Demod.DC_offset));
-    mvwprintw(sdr,2,0,"LNA         %u",Demod.lna_gain);
-    mvwprintw(sdr,3,0,"Mix gain    %u",Demod.mixer_gain);
-    mvwprintw(sdr,4,0,"IF gain     %u",Demod.if_gain);
+    mvwprintw(sdr,2,0,"I/Q imbal%7.1f dB",power2dB(crealf(Demod.power)/cimagf(Demod.power)));
+    mvwprintw(sdr,3,0,"I/Q dot  %7.1f dB",power2dB(Demod.dot));
+    mvwprintw(sdr,4,0,"LNA      %7u",Demod.lna_gain);
+    mvwprintw(sdr,5,0,"Mix gain %7u",Demod.mixer_gain);
+    mvwprintw(sdr,6,0,"IF gain  %7u",Demod.if_gain);
     wrefresh(sdr);
     
-    char str[80];
+
     double f;
     c = getch();
     switch(c){
-    case ERR:
+    case ERR:   // no key; timed out. Do nothing.
       continue;
-    case 'q':
+    case 'q':   // Exit radio program
       goto done;
-    case 'f':
+    case '\f':  // Screen repaint
+      clearok(curscr,TRUE);
+      break;
+    case 'n':   // Set noise reference to current amplitude; hit with no sig
+      Demod.noise = Demod.amplitude;
+      break;
+    case 'f':   // Tune to new frequency
       prompt = newwin(3,50,20,0);
       box(prompt,0,0);
-      mvwprintw(prompt,1,1,"Enter frequency in Hz: ");
+      mvwprintw(prompt,1,1,"Enter frequency in Hz:");
       wrefresh(prompt);
       echo();
       timeout(0);
+      char str[80];
       wgetnstr(prompt,str,sizeof(str));
       timeout(100);
       noecho();
       f = atof(str);
-      set_second_LO(-48000,0);
-      set_first_LO(f - 48000,1);
+      if(f > 0){
+	set_second_LO(-48000,0);
+	set_first_LO(f - 48000,1);
+      }
       werase(prompt);
       wrefresh(prompt);
       delwin(prompt);
