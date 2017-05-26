763a970dea854c67ca45a4186dee1189437ad3c1
karn <karn>
2017-05-26 00:04:22 +0000

    portable integer definitions

--- rtp.h
+++ rtp.h
@@ -1,11 +1,12 @@
 #ifndef _RTP_H
 #define RTP_H 1
+#include <stdint.h>
 struct rtp_header {
-  unsigned char vpxcc;
-  unsigned char mpt;
-  unsigned short seq;
-  unsigned long timestamp;
-  unsigned long ssrc;
+  uint8_t vpxcc;
+  uint8_t mpt;
+  uint16_t seq;
+  uint32_t timestamp;
+  uint32_t ssrc;
 };
 #define RTP_VERS 2
 
