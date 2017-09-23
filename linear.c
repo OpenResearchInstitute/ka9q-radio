57c748a4133c195bc2673336b0b251182919fd02
karn <karn>
2017-09-23 07:39:16 +0000

    major changes reimplement AGC to work on every sample, (per block caused audible AM noise) don't respond instantly to signal peaks; just ramp gain down at specified rate implement envelope detection implement MONO mode (mainly for SSB/CW) fold snr and bb_power calculations into demod loops limit FFT coarse search rate get rid of some modulo operators

--- linear.c
+++ linear.c
@@ -1,4 +1,4 @@
-// $Id: linear.c,v 1.1 2017/09/20 19:47:11 karn Exp karn $
+// $Id: linear.c,v 1.2 2017/09/22 18:16:42 karn Exp karn $
 
 // General purpose linear modes demodulator
 // Derived from dsb.c by folding in ISB and making coherent tracking optional
@@ -25,6 +25,7 @@
 
 static float const hangtime = 1.1;      // AGC Hang for 1.1 seconds after new peak
 static float const recovery_rate = 6;   // AGC recovery rate 6 db/sec after hang finishes
+static float const attack_rate = -30;   // AGC gain decrease 30 dB/sec when over set point
 static float const snrthreshdb = 3;     // Loop lock threshold at +3 dB SNR
 static int   const fftsize = 1 << 16;   // search FFT bin size = 64K = 1.37 sec @ 48 kHz
 static float const damping = M_SQRT1_2; // PLL loop damping factor; 1/sqrt(2) is "critical" damping
@@ -40,8 +41,9 @@ void *demod_linear(void *arg){
 
   // Set derived (and other) constants
   float const samptime = demod->decimate / demod->samprate;  // Time between (decimated) samples
-  float const snrthresh = powf(10,snrthreshdb/10);           // SNR threshold for lock
+
   // FFT search params
+  float const snrthresh = powf(10,snrthreshdb/10);           // SNR threshold for lock
   int   const unlock_limit = unlock_time / samptime;        // Start acquiring after loss of lock
   float const binsize = 1 / (fftsize * samptime);           // FFT bin size, Hz
   int   const lowlimit = round(searchlow / binsize);        // FFT bin indices for search limits
@@ -56,14 +58,14 @@ void *demod_linear(void *arg){
   float const integrator_gain = 1 / tau1;                  // 2pi
   float const tau2 = 2 * damping / natfreq;                // sqrt(2) / 2pi = 1/ (pi*sqrt(2))
   float const prop_gain = tau2 / tau1;                     // sqrt(2)/2
-  float const ramprate = binsize * samptime / integrator_gain;   // sweep at one bin/sec
+  float const ramprate = binsize * samptime / integrator_gain;   // sweep at one bin * natfreq
 
   // AGC
   int hangcount = 0;
-  float const agcratio = dB2voltage(recovery_rate * ((float)demod->L/demod->samprate)); // 6 dB/sec
-  int const hangmax = hangtime * (demod->samprate/demod->L); // 1.1 second hang before gain increase
+  float const recovery_factor = dB2voltage(recovery_rate * samptime); // AGC ramp-up rate/sample
+  float const attack_factor = dB2voltage(attack_rate * samptime);      // AGC ramp-down rate/sample
+  int const hangmax = hangtime / samptime; // samples before AGC increase
 
-  demod->gain = dB2voltage(70.);
   demod->snr = -INFINITY;
 
   // Detection filter
@@ -85,7 +87,7 @@ void *demod_linear(void *arg){
   float integrator = 0;                 // 2nd order loop integrator
   float delta_f = 0;                    // FFT-derived offset
   float ramp = 0;                       // Frequency sweep (do we still need this?)
-  int unlock_time = 0;                  // Time since apparent loss of lock, samples
+  int unlock_time = 0;                  // noisy buffers since last FFT search
   float calibrate_offset = 0;           // Frequency error for calibration mode
 
   while(!demod->terminate){
@@ -99,48 +101,48 @@ void *demod_linear(void *arg){
     else
       demod->n0 = compute_n0(demod); // Happens at startup
 
-    if(demod->flags & COHERENT){
+    if(demod->flags & COHERENT){ // carrier (or regenerated carrier) tracking in coherent mode
       // Copy into circular input buffer for FFT in case we need it
       if(demod->flags & SQUARE){
-	for(int i=0;i<filter->olen;i++)
-	  fftinbuf[fft_ptr+i] = filter->output.c[i] * filter->output.c[i];
-      } else {
-	memcpy(fftinbuf+fft_ptr,filter->output.c,filter->olen*sizeof(*filter->output.c));
-      }
-      fft_ptr = (fft_ptr + filter->olen) % fftsize; // assumes filter->olen divides fftsize, otherwise we'll have a buffer overrun!
-      
-      // If SNR is below threshold, use FFT to find approximate carrier frequency
-      if(demod->snr < snrthresh){
-	unlock_time += filter->olen;
-	if(unlock_time >= unlock_limit){
-	  // Run FFT, look for peak bin
-	  fftwf_execute(fft_plan);
-	  // Search limited range of FFT buffer for peak energy
-	  int maxbin = 0;
-	  float maxenergy = 0;
-	  
-	  for(int n = lowlimit; n <= highlimit; n++){
-	    float const e = cnrmf(fftoutbuf[(n + fftsize) % fftsize]);
-	    if(e > maxenergy){
-	      maxenergy = e;
-	      maxbin = n;
-	    }
-	  }
-	  
-	  delta_f = binsize * maxbin; // cycles per second	
-	  if(demod->flags & SQUARE)
-	    delta_f /= 2; // Squaring loop provides 2x frequency
-	  
-	  coarse_phasor_step = csincos(-phase_scale * delta_f);
-	  if(ramp == 0) // not already sweeping
-	    ramp = ramprate;
+	for(int i=0;i<filter->olen;i++){
+	  fftinbuf[fft_ptr++] = filter->output.c[i] * filter->output.c[i];
+	  if(fft_ptr >= fftsize)
+	    fft_ptr -= fftsize;
 	}
       } else {
-	unlock_time = 0; // Reset
-	ramp = 0;
+	for(int i=0;i<filter->olen;i++){
+	  fftinbuf[fft_ptr++] = filter->output.c[i];
+	  if(fft_ptr >= fftsize)
+	    fft_ptr -= fftsize;
+	}
       }
-      
-      // Apply coarse offset, determine fine frequency adjustment with PLL
+      // If SNR is below threshold, use FFT to find approximate carrier frequency
+      if(demod->snr < snrthresh && ++unlock_time >= unlock_limit){ // Don't do this too often
+	unlock_time = 0;
+	// Run FFT, look for peak bin
+	fftwf_execute(fft_plan);
+	
+	// Search limited range of FFT buffer for peak energy
+	int maxbin = 0;
+	float maxenergy = 0;
+	for(int n = lowlimit; n <= highlimit; n++){
+	  float const e = cnrmf(fftoutbuf[n < 0 ? n + fftsize : n]);
+	  if(e > maxenergy){
+	    maxenergy = e;
+	    maxbin = n;
+	  }
+	}
+	delta_f = binsize * maxbin; // cycles per second	
+	if(demod->flags & SQUARE)
+	  delta_f /= 2; // Squaring loop provides 2x frequency
+	
+	coarse_phasor_step = csincos(-phase_scale * delta_f);
+	if(ramp == 0) // not already sweeping
+	  ramp = ramprate;
+      } else
+	ramp = 0; // Don't ramp when locked
+
+      // Apply coarse offset, track fine frequency adjustment with PLL
       complex float accum = 0;
       for(int n=0;n<filter->olen;n++){
 	filter->output.c[n] *= coarse_phasor * fine_phasor;
@@ -148,21 +150,18 @@ void *demod_linear(void *arg){
 	if(demod->flags & SQUARE){
 	  complex float const ss = filter->output.c[n] * filter->output.c[n];
 	  accum += ss;
-	  //carrier_phase = fast_cargf(ss)/2;
 	  carrier_phase = cargf(ss)/2;
 	} else {
 	  accum += filter->output.c[n];
-	  // Fast (?) approximation to atan2/carg
-	  //carrier_phase = fast_cargf(filter->output.c[n]);
 	  carrier_phase = cargf(filter->output.c[n]);
 	}	
 	// Lag-lead (integral plus proportional)
 	integrator += carrier_phase * samptime + ramp;
 	// Acquisition frequency sweep between +/- binsize/2
 	if((integrator * integrator_gain >= binsize/2) && (ramp > 0))
-	  ramp = -ramprate; // reached sweep limit, toggle ramp direction
+	  ramp = -ramprate; // reached upward sweep limit, sweep down
 	else if((integrator * integrator_gain <= -binsize/2) && (ramp < 0))
-	  ramp = ramprate;
+	  ramp = ramprate;  // Reached downward sweep limit, sweep up
 	
 	float const feedback = integrator_gain * integrator + prop_gain * carrier_phase; // units of Hz
 	// Small angle approximation to csincosf(-phase_scale * feedback); 
@@ -170,6 +169,7 @@ void *demod_linear(void *arg){
 	
 	if((demod->flags & CAL) && demod->snr >= snrthresh){
 	  // In calibrate mode, keep highly smoothed estimate of frequency offset
+	  // Apply this to calibration estimate below
 	  calibrate_offset += .00001 * (feedback + delta_f - calibrate_offset);
 	}
 	demod->foffset += .001 * (feedback + delta_f - demod->foffset);
@@ -184,56 +184,91 @@ void *demod_linear(void *arg){
       
       fine_phasor /= cabsf(fine_phasor);
       coarse_phasor /= cabsf(coarse_phasor);
-    }      
-
-    // compute signal (I) and noise (Q) levels, total level and SNR
-    float totampl;
-    {
-      complex float const p = cpowers(filter->output.c,filter->olen);
-      float const signal = crealf(p);
-      float const noise = cimagf(p);
-      if(isnan(signal) || isnan(noise)){
-	// Startup transient
-	demod->bb_power = 0;
-	demod->snr = -INFINITY;
-	totampl = 0;
-      } else {
-	demod->snr = (signal / noise) - 1; // S/N as power ratio
-	demod->bb_power = signal + noise;
-	totampl = sqrtf(signal + noise);
+      if((demod->flags & CAL) && demod->snr >= snrthresh){
+	// In calibrate mode, apply and clear the current measured offset
+	set_cal(demod,demod->calibrate - calibrate_offset/demod->frequency);
+	calibrate_offset = 0;
       }
-    }
-    if((demod->flags & CAL) && demod->snr >= snrthresh){
-      // In calibrate mode, apply and clear the current measured offset
-      set_cal(demod,demod->calibrate - calibrate_offset/demod->frequency);
-      calibrate_offset = 0;
-    }
+    } // if(COHERENT)
 
-    // Manual frequency shift
-    if(demod->shift != 0){
-      for(int n=0; n < filter->olen; n++){
-	filter->output.c[n] *= demod->shift_phasor;
-	demod->shift_phasor *= demod->shift_phasor_step;
+    if(demod->flags & ENVELOPE){
+      // Envelope detected AM
+      float audio[filter->olen];
+      float power = 0;
+      for(int n=0; n<filter->olen; n++){
+	float const ampsq = cnrmf(filter->output.c[n]);
+	float const amp = sqrtf(ampsq);
+	power += ampsq;
+
+	audio[n] = demod->gain * amp;
+	if(fabsf(audio[n]) > demod->headroom){
+	  demod->gain *= attack_factor;
+	  audio[n] = demod->gain * amp;
+	  hangcount = hangmax;
+	} else if(hangcount != 0){
+	  hangcount--;
+	} else {
+	  demod->gain *= recovery_factor;
+	}
       }
-      demod->shift_phasor /= cabs(demod->shift_phasor);
-    }
-    // Q is on the right channel, so use both I and Q for gain setting so we don't blast our ears when the phase is wrong
-    if(demod->gain * totampl > demod->headroom){ // Target to about -10 dBFS
-      // New signal peak: decrease gain and inhibit re-increase for a while
-      if(totampl != 0)
-	demod->gain = demod->headroom / totampl;
-      hangcount = hangmax;
+      demod->bb_power = power / filter->olen;
+      send_mono_audio(demod->audio,audio,filter->olen);
     } else {
-      // Not a new peak, but the AGC is still hanging at the last peak
-      if(hangcount != 0){
-	hangcount--;
-      } else {
-	// OK to increase gain; should enforce a limit
-	demod->gain *= agcratio;
+      // All other linear modes
+      // Manual frequency shift
+      if(demod->shift != 0){
+	for(int n=0; n < filter->olen; n++){
+	  filter->output.c[n] *= demod->shift_phasor;
+	  demod->shift_phasor *= demod->shift_phasor_step;
+	}
+	demod->shift_phasor /= cabs(demod->shift_phasor);
       }
-    }
-    send_stereo_audio(demod->audio,filter->output.c,filter->olen,demod->gain);
-  }
+      float signal = 0;
+      float noise = 0;
+
+      if(demod->flags & MONO){
+	// Send only I channel as mono
+	float audio[filter->olen];
+	for(int n=0; n<filter->olen; n++){
+	  signal += crealf(filter->output.c[n]) * crealf(filter->output.c[n]);
+	  noise += cimagf(filter->output.c[n]) * cimagf(filter->output.c[n]);
+	  audio[n] = demod->gain * crealf(filter->output.c[n]);
+	  if(fabs(audio[n]) > demod->headroom){
+	    demod->gain *= attack_factor;
+	    audio[n] = demod->gain * crealf(filter->output.c[n]);
+	  } else if(hangcount != 0){
+	    hangcount--;
+	  } else {
+	    demod->gain *= recovery_factor;
+	  }
+	}
+	send_mono_audio(demod->audio,audio,filter->olen);
+      } else { // send I & Q as stereo
+	complex float audio[filter->olen];
+	for(int n=0; n<filter->olen; n++){
+	  signal += crealf(filter->output.c[n]) * crealf(filter->output.c[n]);
+	  noise += cimagf(filter->output.c[n]) * cimagf(filter->output.c[n]);
+	  
+	  audio[n] = demod->gain * filter->output.c[n];
+	  if(cnrmf(audio[n]) > demod->headroom * demod->headroom){
+	    demod->gain *= attack_factor;
+	    audio[n] = demod->gain * filter->output.c[n];
+	    hangcount = hangmax;
+	  } else if(hangcount != 0){
+	    hangcount--;
+	  } else {
+	    demod->gain *= recovery_factor;
+	  }
+	}
+	send_stereo_audio(demod->audio,audio,filter->olen);
+      }
+      demod->bb_power = (signal + noise) / filter->olen;
+      if(demod->flags & COHERENT)
+	demod->snr = (signal / noise) - 1; // S/N as power ratio; meaningful only in coherent modes
+      else
+	demod->snr = NAN;
+    } // not envelope detection
+  } // terminate
   fftwf_free(fftinbuf);
   fftwf_free(fftoutbuf);  
   fftwf_destroy_plan(fft_plan);
