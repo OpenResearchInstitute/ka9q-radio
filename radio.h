2d4416833f95c919d19e6ca12356581591dd2def
karn <karn>
2017-10-21 01:56:18 +0000

    *** empty log message ***

--- radio.h
+++ radio.h
@@ -1,4 +1,4 @@
-// $Id: radio.h,v 1.53 2017/10/01 23:25:55 karn Exp karn $
+// $Id: radio.h,v 1.54 2017/10/10 12:20:51 karn Exp karn $
 #ifndef _RADIO_H
 #define _RADIO_H 1
 
@@ -10,17 +10,6 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
-
-#define CTLPORT 7300
-
-// Modetab flags
-#define CONJ 1      // Cross-conjugation of positive and negative frequencies, for ISB
-#define FLAT 2      // No baseband filtering for FM
-#define COHERENT 4  // Coherent carrier tracking
-#define CAL 8       // Calibrate mode in coherent demod; adjust calibrate rather than frequency
-#define SQUARE   16 // Square carrier in coherent loop (BPSK/suppressed carrier AM)
-#define ENVELOPE 32 // Envelope detection of AM
-#define MONO     64 // Only output I channel of linear mode
 
 struct modetab {
   char name[16];
@@ -68,6 +57,7 @@ struct demod {
 
   struct status requested_status; // The status we want the FCD to be in
   struct status status;           // Last status from FCD
+  int tuner_lock;                 // When set, don't try to command tuner
   // 'status' is written by the input thread and read by set_first_LO, etc, so it's protected by a mutex
   pthread_mutex_t status_mutex;
   pthread_cond_t status_cond;     // Signalled whenever status changes
@@ -92,6 +82,15 @@ struct demod {
   char demod_name[16];
   int terminate;                  // set to 1 by set_mode() to request graceful termination
   int flags;                      // Special flags to demodulator
+// Modetab flags
+#define CONJ 1      // Cross-conjugation of positive and negative frequencies, for ISB
+#define FLAT 2      // No baseband filtering for FM
+#define COHERENT 4  // Coherent carrier tracking
+#define CAL 8       // Calibrate mode in coherent demod; adjust calibrate rather than frequency
+#define SQUARE   16 // Square carrier in coherent loop (BPSK/suppressed carrier AM)
+#define ENVELOPE 32 // Envelope detection of AM
+#define MONO     64 // Only output I channel of linear mode
+
   double start_freq;              // Initial frequency to set at startup
 
   // Tuning parameters
@@ -132,7 +131,6 @@ struct demod {
   complex double shift_phasor;
   complex double shift_phasor_step;
 
-  int frequency_lock; // inhibits tuning of RF and LO & IF tuning operate in lockstep
   int tunestep;       // User interface cursor location, log10(); e.g., 3 -> thousands
 
   // Experimental notch filter
