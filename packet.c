5c5cf5aaaef458db10c63bb921776501da888722
karn <karn>
2018-02-06 01:36:16 +0000

    *** empty log message ***

--- packet.c
+++ packet.c
@@ -1,4 +1,4 @@
-// $Id: packet.c,v 1.1 2018/01/24 07:04:51 karn Exp karn $
+// $Id: packet.c,v 1.2 2018/01/25 20:05:08 karn Exp karn $
 // AFSK/FM packet demodulator
 
 #define _GNU_SOURCE 1
@@ -7,16 +7,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <limits.h>
 #include <string.h>
-#include <sys/time.h>
-#include <sys/socket.h>
-#include <sys/resource.h>
-#include <netdb.h>
 #include <locale.h>
 #include <errno.h>
+#include <ctype.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 #include "filter.h"
 #include "misc.h"
@@ -48,28 +44,22 @@ struct packet {
 
 
 
-char Mcast_address_text[] = "239.2.1.1";
+char *Mcast_address_text = "audio-pcm-mcast.local";
 float const SCALE = 1./32768;
 int const Bufsize = 2048;
 int const AN = 2048; // Should be power of 2 for FFT efficiency
 int const AL = 1000; // 25 bit times
-//int const AM = AN - AL + 1; // should be >= 40, i.e., samprate / bitrate
+//int const AM = AN - AL + 1; // should be >= Samppbit, i.e., samprate / bitrate
 int const AM = 1049;
-
-extern float Kaiser_beta;
-
 float const Samprate = 48000;
 float const Bitrate = 1200;
 //int const Samppbit = Samprate/Bitrate;
 int const Samppbit = 40;
 
 int Input_fd = -1;
-
-
-
-
-
 struct packet *Packet;
+extern float Kaiser_beta;
+int Verbose;
 
 struct packet *lookup_session(const struct sockaddr *sender,const uint32_t ssrc){
   struct packet *sp;
@@ -127,6 +117,126 @@ int close_session(struct packet *sp){
   return 0;
 }
 
+
+int dump_frame(unsigned char *frame,int bytes){
+
+  // By default, no digipeaters; will update if there are any
+  unsigned char *control = frame + 14;
+
+  // Source address
+  // Is this the transmitter?
+  int this_transmitter = 1;
+  int digipeaters = 0;
+  // Look for digipeaters
+  if(!(frame[13] & 1)){
+    // Scan digipeater list; have any repeated it?
+    for(int i=0;i<8;i++){
+      unsigned char digi_ssid = frame[20 + 7*i];
+      
+      digipeaters++;
+      if(digi_ssid & 0x80){
+	// Yes, passed this one, keep looking
+	this_transmitter = 2 + i;
+      }
+      if(digi_ssid & 1)
+	break; // Last digi
+    }
+  }
+  // Show source address, in upper case if this is the transmitter, otherwise lower case
+  for(int n=7; n < 13; n++){
+    unsigned char c = frame[n] >> 1;
+    if(c == ' ')
+      break;
+    if(this_transmitter == 1)
+      putchar(toupper(c));
+    else
+      putchar(tolower(c));
+  }
+  printf("-%d",(frame[13] >> 1) & 0xf); // SSID
+  
+  printf(" -> ");
+  
+  // List digipeaters
+
+  if(!(frame[13] & 0x1)){
+    // Digipeaters are present
+    for(int i=0; i<digipeaters; i++){
+      for(int k=0;k<6;k++){
+	unsigned char c = frame[14 + 7*i + k] >> 1;
+	if(c == ' ')
+	  break;
+	
+	if(this_transmitter == 2+i)
+	  putchar(toupper(c));
+	else
+	  putchar(tolower(c));
+      }
+      int ssid = frame[14 + 7*i + 6];
+      printf("-%d",(ssid>> 1) & 0xf); // SSID
+      printf(" -> ");
+      if(ssid  & 0x1){ // Last one
+	control = frame + 14 + 7*i + 7;
+	break;
+      }
+    }
+  }
+  // NOW print destination, in lower case since it's not the transmitter
+  for(int n=0; n < 6; n++){
+    unsigned char c = frame[n] >> 1;
+    if(c == ' ')
+      break;
+    putchar(tolower(c));
+  }
+  printf("-%d",(frame[6] >> 1) & 0xf); // SSID
+
+  // Type field
+  printf("; control = %02x",*control++);
+  printf("; type = %02x\n",*control);
+
+  for(int i = 0; i < bytes; i++){
+    printf("%02x ",frame[i]);
+    if((i % 16) == 15 || i == bytes-1){
+      for(int k = (i % 16); k < 15; k++)
+	printf("   "); // blanks as needed in last line
+
+      printf(" |  ");
+      for(int k=(i & ~0xf );k <= i; k++){
+	if(isprint(frame[k]))
+	  putchar(frame[k]);
+	else
+	  putchar('.');
+      }
+      printf("\n");
+    }
+  }
+  printf("\n");
+  return 0;
+}
+
+// Check CRC on frame
+int crc_good(unsigned char *frame,int length){
+  unsigned int const crc_poly = 0x8408;
+	
+  unsigned short crc = 0xffff;
+  while(length-- > 0){
+    unsigned char byte = *frame++;
+    for(int i=0; i < 8; i++){
+      unsigned short feedback = 0;
+      if((crc ^ byte) & 1)
+	feedback = crc_poly;
+
+      crc = (crc >> 1) ^ feedback;
+      byte >>= 1;
+    }
+  }
+  if(crc == 0xf0b8)
+    return 1;
+  else
+    return 0;
+}
+
+
+
 // Packet demod task - incomplete
 void *decode_task(void *arg){
   pthread_setname("decode");
@@ -134,56 +244,21 @@ void *decode_task(void *arg){
   assert(sp != NULL);
 
   struct filter_out *filter = create_filter_output(sp->filter_in,NULL,1,COMPLEX);
-  set_filter(filter,Samprate,+300,+3300,3.0); // Creates analytic, band-limited signal from +900 to +2500 Hz
+  set_filter(filter,Samprate,+100,+4000,3.0); // Creates analytic, band-limited signal
 
-#if 0
-
-  // Set up sync correlator
-  int const sync_length = 9 * Samppbit; // Samples in a sync vector
-  float *timebuf = fftwf_alloc_real(1024);
-  float complex *fbuf = fftwf_alloc_complex(513);
-  fftwf_plan plan = fftwf_plan_dft_r2c_1d(1024,timebuf,fbuf,FFTW_ESTIMATE);
-  // Fill timebuf with prototype of flag pattern: 1 bit time of 0, 7 bits of 1, 1 bit of 0
-  // This includes the effect of NRZI encoding
-  memset(timebuf,0,1024*sizeof(*timebuf));
-  int i;
-  for(i=0;i<Samppbit;i++)
-    timebuf[i] = -1;
-
-  for(;i<8*Samppbit;i++)
-    timebuf[i] = +1;
-
-  for(;i<9*Samppbit;i++)
-    timebuf[i] = -1;
-  
-  fftwf_execute(plan); // To frequency domain
-  fftwf_destroy_plan(plan);
-  fftwf_free(timebuf);
-  timebuf = NULL;
-
-  
-  struct filter_in *sync_in = create_filter_input(664,sync_length+1,REAL);
-  struct filter_out *sync_out = create_filter_output(sync_in,fbuf,1,REAL);
-
-
-
-
-  complex float memory = 0;
-  float last_val = 0;
-  int samp_since_last_edge = 0;
+  int bits_since_last_edge = 0;
   unsigned char hdlc_frame[1024];
   memset(hdlc_frame,0,sizeof(hdlc_frame));
   int frame_bit = 0;
-  unsigned short crc = 0xffff;
   int zero_pending = 0;
-  int sync_corr_count = 0;
+  float last_val = 0;
+  int packets = 0;
 
-#endif
-  int sample = 0;
-  float complex mark_delay_line[40];
+
+  float complex mark_delay_line[Samppbit];
   memset(mark_delay_line,0,sizeof(mark_delay_line));
   int pointer = 0;
-  float complex space_delay_line[40];
+  float complex space_delay_line[Samppbit];
   memset(space_delay_line,0,sizeof(space_delay_line));
   float complex mark_phase = 1;
   float complex mark_step = csincosf(-2*M_PI*1200./Samprate);
@@ -191,166 +266,189 @@ void *decode_task(void *arg){
   float complex space_step = csincosf(-2*M_PI*2200./Samprate);
   float complex mark_sum = 0;
   float complex space_sum = 0;
-
+  int flagsync = 0;
+#if 0
+  int sample = 0;
   printf("signed double\n");
+#endif
 
+  int ones = 0;
+  int zeroes = 0;
+
+  float peak_mark = 0;
+  float peak_space = 0;
+  float const peak_smooth = .001;
+  float old_y[Samppbit];
+  memset(old_y,0,sizeof(old_y));
 
   while(1){
     execute_filter_output(filter);    // Blocks until data appears
 
+    float y[filter->olen];
+    float phase_energies[Samppbit];
 
+    memset(phase_energies,0,sizeof(phase_energies));
+    int pointer_start = pointer;
     for(int n=0; n<filter->olen; n++){
+
+      // Spin down by 1200 and 2200 Hz, accumulate each in a sliding boxcar (comb) filter
       float complex s =  mark_phase * filter->output.c[n];
       mark_phase *= mark_step;
-#if 0
-      if(isnan(crealf(s)) || isnan(cimagf(s)))
-	s = 0;
-#endif
+      mark_sum -= mark_delay_line[pointer]; // Remove old sample falling off back end of boxcar
+      mark_sum += mark_delay_line[pointer] = s; // And add in the new one
+	peak_mark *= (1-peak_smooth);
+      if(cnrmf(mark_sum) >= peak_mark)
+	peak_mark = cnrmf(mark_sum);
 
-      mark_sum -= mark_delay_line[pointer];
-      mark_sum += mark_delay_line[pointer] = s;
 
       s =  space_phase * filter->output.c[n];
       space_phase *= space_step;
-#if 0
-      if(isnan(crealf(s)) || isnan(cimagf(s)))
-	s = 0;
-#endif
-
       space_sum -= space_delay_line[pointer];
       space_sum += space_delay_line[pointer] = s;
-      pointer = (pointer + 1) % 40;
+      peak_space *= (1-peak_smooth);
+      if(cnrmf(space_sum) >= peak_space)
+	peak_space = cnrmf(space_sum);
 
-      float y = cnrmf(mark_sum) - cnrmf(space_sum);
-
-      printf(". %d %f\n",sample++,y);
+      // Noncoherent FSK detection: which tone channel has more energy?
+      y[n] = cnrmf(mark_sum) - cnrmf(space_sum);
+      phase_energies[pointer] += fabsf(y[n]);
       
-      
+      if(++pointer >= Samppbit)
+	pointer = 0;
+    }
+    mark_phase = mark_phase / cabsf(mark_phase);
+    space_phase = space_phase / cabsf(space_phase);
 
+    // Which phase is best?
+    float max_signal = 0;
+    int max_n = -1;
+    for(int n=0;n < Samppbit;n++){
+      if(phase_energies[n] >= max_signal){
+	max_signal = phase_energies[n];
+	max_n = n;
+      }
+    }
 
+    // Now actually process the demodulated data
+    for(int i = max_n - pointer_start; i < filter->olen; i += Samppbit){
+      float yval;
 
-#if 0
-      // FM demodulate
-      float y = cargf(filter->output.c[n] * conjf(memory)) - (2 * M_PI * 1700. / Samprate); // center is 1700 Hz;
-      memory = filter->output.c[n];
+      if(i < 0)
+	yval = old_y[i + Samppbit];
+      else
+	yval = y[i];
 
-#if 0
-
-      sync_in->input.r[sync_corr_count++] = y;
-      if(sync_corr_count == sync_in->ilen){
-	execute_filter_input(sync_in);
-	sync_corr_count = 0;
-	execute_filter_output(sync_out);
-	for(int k=0;k<sync_out->olen;k++){
-	  printf(". %d %f\n",sample++,sync_out->output.r[k]);
+#if 1
+      assert(frame_bit >= 0);
+      if(yval * last_val < 0){
+	// NRZI zero
+	if(ones == 6){
+	  // Flag
+	  if(flagsync){
+	    frame_bit -= 7; // Remove 0111111
+	    int bytes = frame_bit / 8;
+	    if(bytes > 0 && crc_good(hdlc_frame,bytes)){
+	      printf("Packet %d, %d bytes\n",packets++,bytes);
+	      dump_frame(hdlc_frame,bytes);
+	    }
+	  }
+	  if(1 || frame_bit != 0){
+	    memset(hdlc_frame,0,sizeof(hdlc_frame));
+	    frame_bit = 0;
+	  }
+	  flagsync = 1;
+	} else if(ones == 5){
+	  // Drop stuffed zero
+	} else if(ones < 5){
+	  if(flagsync){
+	    frame_bit++;
+	  }
+	}
+	ones = 0;
+      } else {
+	// NRZI one
+	if(++ones == 7){
+	  // Abort
+	  if(1 || frame_bit != 0){
+	    memset(hdlc_frame,0,sizeof(hdlc_frame));
+	    frame_bit = 0;
+	  }
+	  flagsync = 0;
+	} else {
+	  if(flagsync){
+	    hdlc_frame[frame_bit/8] |= 1 << (frame_bit % 8);
+	    frame_bit++;
+	  }
 	}
       }
-#endif
+#else
 
-#if 0
-
-      if(y * last_val < 0){ // NRZI decode: sign flip = 0, no sign flip = 1
-	//	int const crc_poly = 0x1021;
-	int const crc_poly = 0x8408;
+      if(yval * last_val < 0){ // NRZI decode: sign flip = 0, no sign flip = 1
 
 	// See how many 1 bits we've had since last zero
 	// 4 or less: that many 1's followed by zero
 	// 5: five 1's, then drop next 0
 	// 6: six 1's: flag, end frame
 	// 7 or more: abort frame
-	int one_bits = roundf((float)samp_since_last_edge / Samppbit) - 1;
+	int one_bits = bits_since_last_edge;
+	bits_since_last_edge = 0;
 
-	if(one_bits >= 0 && one_bits <= 5){
+	if(flagsync && (one_bits >= 0 && one_bits <= 5) && (frame_bit < 8 * 1022)){ // Leave room
+	  // not a flag or abort
 	  // Previous transition was a zero bit
 	  if(zero_pending){
 	    // Insert single zero
-	    if((crc & 1) ^ 0)
-	      crc = (crc >> 1) ^ crc_poly;
-	    else
-	      crc >>= 1;
 	    frame_bit++;
 	  }
 	  // Insert 'one_bits' ones
-	  for(int i=0;i<one_bits;i++){
+	  for(int k=0;k<one_bits;k++){
 	    hdlc_frame[frame_bit/8] |= 1 << (frame_bit % 8);
-	    if((crc & 1) ^ 1)
-	      crc = (crc >> 1) ^ crc_poly;
-	    else
-	      crc >>= 1;
 	    frame_bit++;
 	  }
 	  if(one_bits < 5)
 	    zero_pending = 1;
 	  else
-	    zero_pending = 0;
+	    zero_pending = 0;  // remove bit-stuffed 0
 
 	} else if(one_bits == 6) {
-	  // Flag
+	  // Flag began with the preceeding zero
 	  // Check CRC, process frame
-	  if(frame_bit > 0)
-	    printf("FLAG, crc = %x (%s), length = %d bytes %d bits\n",crc,
-		   crc == 0xf0b8? "good" : "bad",
-		   frame_bit/8,frame_bit%8);
-
-	  if(crc == 0xf0b8){
-	    int bytes = frame_bit / 8;
-	    for(int i = 0; i < bytes; i++){
-	      printf("%02x ",hdlc_frame[i]);
-	      if((i % 16) == 15)
-		printf("\n");
-	    }
-	    printf("\n");
-	  }
-
-	  memset(hdlc_frame,0,sizeof(hdlc_frame));
-	  frame_bit = 0;
-	  zero_pending = 0;
-	  crc = 0xffff;
-	} else if(one_bits >= 7){
-	  // Abort
-	  memset(hdlc_frame,0,sizeof(hdlc_frame));
-	  frame_bit = 0;
-	  zero_pending = 0;
-	  crc = 0xffff;
-	}
-	samp_since_last_edge = 0;
-      } else {
-	// No transition - string of 1 bits
-	samp_since_last_edge++;
-	// 7 or more consecutive 1's: frame abort
-      }
-      last_val = y;
-#endif
-#endif
-
-    }
-    mark_phase = mark_phase / cabsf(mark_phase);
-    space_phase = space_phase / cabsf(space_phase);
-
+	  if(flagsync){
+	    
+	    if(frame_bit > 0){
 #if 0
-    // Find symbol timing
-    int best_symtime = -1;
-    float best_energy = 0;
-    for(int symtime = 0; symtime < Samppbit;symtime++){
-      // Find total energies at each offset
-      float e = 0;
-      for(int i=symtime;i<mark_filter->olen;i+= Samppbit)
-	e += cnrmf(mark_filter->output.c[i]) + cnrmf(space_filter->output.c[i]);
-
-      if(e > best_energy){
-	best_energy = e;
-	best_symtime = symtime;
+	      printf("crc = %x (%s), length = %d bytes %d bits\n",crc,
+		     crc == 0xf0b8? "good" : "bad",
+		     frame_bit/8,frame_bit%8);
+#endif	      
+	      int bytes = frame_bit / 8;
+              if(crc_good(hdlc_frame,bytes)){
+		printf("Packet %d, %d bytes\n",packets++,bytes);
+		dump_frame(hdlc_frame,bytes);
+	      }
+	    }
+	  }
+	  flagsync = 1;
+	  memset(hdlc_frame,0,sizeof(hdlc_frame));
+	  frame_bit = 0;
+	  zero_pending = 0;
+	} else if(one_bits > 6){
+	  // Abort
+	  flagsync = 0;
+	  memset(hdlc_frame,0,sizeof(hdlc_frame));
+	  frame_bit = 0;
+	  zero_pending = 0;
+	}
+      } else {
+	bits_since_last_edge++;
       }
-    }
-    if(best_symtime == -1){
-      // No energy at all; discard
-      continue;
-    }
 #endif
 
+
+      last_val = yval;
+    }
+    memcpy(old_y,&y[filter->olen - Samppbit],sizeof(old_y));
   }
-
-
 
   return NULL;
 
@@ -358,9 +456,25 @@ void *decode_task(void *arg){
 
 
 int main(int argc,char *argv[]){
+  setlocale(LC_ALL,getenv("LANG"));
 
+  int c;
+  while((c = getopt(argc,argv,"I:v")) != EOF){
+    switch(c){
+    case 'v':
+      Verbose++;
+      break;
+    case 'I':
+      Mcast_address_text = optarg;
+      break;
+    default:
+      fprintf(stderr,"Usage: %s [-v] [-I mcast_address]\n",argv[0]);
+      fprintf(stderr,"Defaults: %s -I %s\n",argv[0],Mcast_address_text);
+      exit(1);
+    }
+  }
+  
 
-  // Initialize these here
 
   // Set up multicast input
   Input_fd = setup_mcast(Mcast_address_text,0);
@@ -461,7 +575,6 @@ int main(int argc,char *argv[]){
       signed short *dp = data;
       while(samples-- > 0){
 	// Swap sample to host order, convert to float
-
 	sp->filter_in->input.r[sp->input_pointer++] = ntohs(*dp++) * SCALE;
 	if(sp->input_pointer == sp->filter_in->ilen){
 	  execute_filter_input(sp->filter_in); // Wakes up any threads waiting for data on this filter
