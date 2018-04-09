6f67885d0947b202af1cfc7401dd87306cfe966c
karn <karn>
2018-04-09 21:25:59 +0000

    new rtp structure and conversion routines

--- multicast.h
+++ multicast.h
@@ -6,18 +6,23 @@ extern int Mcast_ttl;
 
 struct rtp_header {
   uint8_t vpxcc;
-  uint8_t mpt;
+  uint8_t type;
   uint16_t seq;
   uint32_t timestamp;
   uint32_t ssrc;
+  int marker;
 };
+#define RTP_MIN_SIZE 12  // min size of RTP header
 #define RTP_VERS 2
 #define RTP_MARKER 0x80  // Marker flag in mpt field
 
+#define IQ_PT (97) // NON-standard payload type for my raw I/Q streams
 #define PCM_MONO_PT (11)
 #define PCM_STEREO_PT (10)
-// Hard-coded RTP payload type for OPUS (NOT STANDARD! should be dynamic with sdp)
-#define OPUS_PT (111)
 
+#define OPUS_PT (111) // Hard-coded NON-standard payload type for OPUS (should be dynamic with sdp)
+
+unsigned char *ntoh_rtp(struct rtp_header *,unsigned char *);
+unsigned char *hton_rtp(unsigned char *, struct rtp_header *);
 
 #endif
