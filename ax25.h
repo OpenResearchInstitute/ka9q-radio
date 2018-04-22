75d543d2e5b98a633b0b271c7a1b0070cfc0ea07
karn <karn>
2018-04-22 22:32:59 +0000

    comments added

--- ax25.h
+++ ax25.h
@@ -1,3 +1,7 @@
+// $Id$
+// Functions defined in ax25.c
+// Copyright 2018, Phil Karn, KA9Q
+
 int dump_frame(unsigned char *frame,int bytes);
 int crc_good(unsigned char *frame,int length);
 char *get_callsign(char *result,unsigned char *in);
