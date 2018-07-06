a46068c178b78da803e9f53ed5c76dcb2498b764
karn <karn>
2018-07-06 06:16:28 +0000

    declaration for F3 filter from Goodman/Carey

--- decimate.h
+++ decimate.h
@@ -8,6 +8,7 @@ struct hb15_state {
   float old_odd_samples[4];
 };
 void hb15_block(struct hb15_state *state,float *output,float *input,int cnt);
+void hb3_block(float *state,float *output,float *input,int cnt);
 
 
 #endif
